d340f653c6704e024431def41b609917e11d696f
Sebastiano2002 <151881700+Sebastiano2002@users.noreply.github.com>
2025-05-05 17:53:26 +0200

    Update train_aspect_extractor.py

--- our_pipeline/train_aspect_extractor.py
+++ our_pipeline/train_aspect_extractor.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='bert-base-uncased')
 parser.add_argument('-d', '--dataset', type=str, default='semeval15')
-parser.add_argument('--epochs', type=int, default=7)
+parser.add_argument('--epochs', type=int, default=2)
 args = parser.parse_args()
 
 output_path = 'data/trained_models/aspect_extractor_{}'.format(args.dataset)
